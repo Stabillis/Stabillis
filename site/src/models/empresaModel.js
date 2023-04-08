673f94d6d0db88d6ceed8a90a832e1c0ba5edaa3
RaulFOliveira <raul.oliveira@sptech.school>
2023-04-08 16:36:18 -0300

    add todos os valores do banco

--- site/src/models/empresaModel.js
+++ site/src/models/empresaModel.js
@@ -12,20 +12,20 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM Empresa WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, telefoneFixo, cep, complemento, cidade, cnpj, logradouro, bairro, estado) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, telefoneFixo, cep, complemento, cidade, cnpj, logradouro, bairro, estado);
+function cadastrar(nome, email, telefoneFixo, cep, complemento, cidade, cnpj, logradouro, bairro, estado, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, telefoneFixo, cep, complemento, cidade, cnpj, logradouro, bairro, estado, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO Empresa (NomeEmpresa, CNPJ, TelefoneFixo, CEP, Logradouro, Complemento, Bairro, Cidade, Estado) VALUES ('${nome}', '${cnpj}', '${telefoneFixo}', '${cep}', '${logradouro}', '${complemento}', '${bairro}', '${cidade}', '${estado}');`;
+        INSERT INTO Empresa (NomeEmpresa, CNPJ, Email, TelefoneFixo, CEP, Logradouro, Complemento, Bairro, Cidade, Estado, Senha) VALUES ('${nome}', '${cnpj}', '${email}', '${telefoneFixo}', '${cep}', '${logradouro}', '${complemento}', '${bairro}', '${cidade}', '${estado}', '${senha}');`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
